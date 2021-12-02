1c702b450b4e3ded2a4594b097307ec5be3a33f8
Mariana Navarro <maronare10@gmail.com>
2021-12-02 18:05:21 -0500

    creando endpoint para listar servicios

--- backend/vivirbien/api/views.py
+++ backend/vivirbien/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import serializers
 
 # Create your views here.
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Servicio
+from .serializers import ServicioSerializer
+
+
+@api_view(['GET'])
+def servicios(request):
+    listaServicios = Servicio.objects.all()
+    serializersServicios = ServicioSerializer(listaServicios, many=True)
+    return Response(serializersServicios.data)
