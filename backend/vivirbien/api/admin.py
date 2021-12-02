7a7b808e29d7f4ec48385a1182efde2eabba6673
Mariana Navarro <maronare10@gmail.com>
2021-12-02 17:40:06 -0500

    actualizando el archivo models.py, creando todos los modelos

--- backend/vivirbien/api/admin.py
+++ backend/vivirbien/api/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Condominio, Edificio
+from .models import Condominio, Edificio, Departamento, Servicio, Pago
 
 
 # Register your models here.
 
 admin.site.register(Condominio)
 admin.site.register(Edificio)
+admin.site.register(Departamento)
+admin.site.register(Servicio)
+admin.site.register(Pago)
+
--- backend/vivirbien/api/models.py
+++ backend/vivirbien/api/models.py
@@ -1,28 +1,55 @@
 from django.db import models
-from django.db.models.fields.related import ForeignKey
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 
 class Condominio (models.Model):
     nombre = models.CharField(max_length=50)
 
+    propietario = models.ForeignKey(User, on_delete=models.RESTRICT)
+
     def __str__(self):
         return self.nombre
 
+
 class Edificio (models.Model):
-  nombre = models.CharField(max_length=30)
-  departamentos = models.PositiveSmallIntegerField(default=0)
-  pisos = models.PositiveSmallIntegerField(default=0)
+    nombre = models.CharField(max_length=30)
+    departamentos = models.PositiveSmallIntegerField(default=0)
+    pisos = models.PositiveSmallIntegerField(default=0)
 
-  condominio = models.ForeignKey(Condominio, related_name='condminios', on_delete=models.CASCADE)
+    condominio = models.ForeignKey(Condominio, related_name='condminios', on_delete=models.CASCADE)
 
-  def __str__(self):
-    return self.nombre
-
-# class Departamento (models.Model):
+    def __str__(self):
+        return self.nombre
 
 
-      
+class Departamento (models.Model):
+    numero = models.PositiveSmallIntegerField(default=0)
+    piso = models.PositiveSmallIntegerField(default=0)
 
+    edificios = models.ForeignKey(Edificio, related_name='edificios', on_delete=models.CASCADE)
+    propietarios = models.ManyToManyField(User)
 
+    def __str__(self):
+        return self.numero
+
+class Servicio (models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Pago (models.Model):
+    monto_a_pagar = models.DecimalField(max_digits=9,decimal_places=2)
+    monto_pagado = models.DecimalField(max_digits=9,decimal_places=2)
+    numero_operacion = models.CharField(max_length=30)
+    vencimiento = models.DateTimeField(null=True, blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now_add=True)
+    
+
+    departamento = models.ForeignKey(Departamento, related_name='pago_departamentos', on_delete=models.CASCADE)
+    propietario = models.ForeignKey(User, related_name='pago_propietarios', on_delete=models.CASCADE)
+    servicio = models.ForeignKey(Servicio, related_name='pago_servicios', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.propietario
