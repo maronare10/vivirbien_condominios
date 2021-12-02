2534ad75262a23e2a6e96a1e548ad185184cf565
Mariana Navarro <maronare10@gmail.com>
2021-12-02 15:53:07 -0500

    Creando modelo Condominio y Edificio

--- backend/vivirbien/api/admin.py
+++ backend/vivirbien/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Condominio, Edificio
+
+
 # Register your models here.
+
+admin.site.register(Condominio)
+admin.site.register(Edificio)
--- backend/vivirbien/api/models.py
+++ backend/vivirbien/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+
+class Condominio (models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Edificio (models.Model):
+  nombre = models.CharField(max_length=30)
+  departamentos = models.PositiveSmallIntegerField(default=0)
+  pisos = models.PositiveSmallIntegerField(default=0)
+
+  condominio = models.ForeignKey(Condominio, related_name='condminios', on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.nombre
+
+# class Departamento (models.Model):
+
+
+      
+
+
