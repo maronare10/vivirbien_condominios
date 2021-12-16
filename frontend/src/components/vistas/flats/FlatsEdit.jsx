668e361b05b64481b8b1e1ef0de67fc4e6cb5b60
Mariana Navarro <maronare10@gmail.com>
2021-12-16 02:58:33 -0500

    frontend: se pueden editar los departementos

--- frontend/src/components/vistas/flats/FlatsEdit.jsx
+++ frontend/src/components/vistas/flats/FlatsEdit.jsx
@@ -3,82 +3,103 @@ import React, { useState, useEffect } from 'react'
 
 import { useHistory, useParams } from "react-router-dom";
 
-import useApp from "../../../server/useApp";
-
 import './FlatsEdit.scss'
 
 const FlatsEdit = () => {
   const historial = useHistory()
 
-  const { buildings, users } = useApp()
-
   const { id } = useParams()
 
-  const url = `http://localhost:8000/flats/${id}`
+  const [edificiosData, setEdificios] = useState([])
+  const [propietariosData, setPropietarios] = useState([])
 
-  const [building, setBuilding] = useState(null)
-  const [user, setUser] = useState(null)
-  const [floor, setFloor] = useState(0)
-  const [number, setNumber] = useState(0)
+  const [datos, setDatos] = useState({
+    piso: 0,
+    numero: 0,
+    edificio: null,
+    propietarios: []
+  });
+
+  const [errors, setErrors] = useState(null)
+
+  const { piso, numero, edificio, propietarios } = datos;
+  
+  const url_departamento = `http://localhost:8000/api/departamentos/${id}`
 
   useEffect(() => {
+    const url_edificios = `http://localhost:8000/api/edificios`
+    const url_propietarios = `http://localhost:8000/api/propietarios`
+    
+    const token = localStorage.getItem('token');
+    const headers = {
+    'Authorization': `Bearer ${token}`}
+    
+    const config_edificios = { method: 'GET', url: url_edificios, headers }
+    const config_propietarios = { method: 'GET', url: url_propietarios, headers } 
+    const config_departamento = { method: 'GET', url: url_departamento, headers }
 
-    axios
-      .get(url)
-      .then(response => {
-        setBuilding(response.data.buildingId)
-        setUser(response.data.userId)
-        setFloor(response.data.floor)
-        setNumber(response.data.number)
+    axios.request(config_edificios)
+      .then((response) => {
+        setEdificios(response.data.results)
       })
+      .catch((error) => {
+        console.log(error)
+      });
+    
+    axios.request(config_propietarios)
+      .then((response) => {
+        setPropietarios(response.data.results)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
 
-  }, [url])
+    axios.request(config_departamento)
+      .then((response) => {
+        const { piso, numero, edificio, propietarios } = response.data
+        setDatos({ piso, numero, edificio, propietarios })
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }, [])
 
-  function currentDate() {
-    const now = new Date()
-    const day = now.getDate()
-    const month = now.getMonth()
-    const year = now.getFullYear()
-    return `${year}-${month}-${day}`
-  }
+  const actualizarState = (e) => {
+    let value = e.target.value
+    if (e.target.multiple) {
+      value = Array.from(
+        e.target.selectedOptions, option => Number(option.value)
+      )
+    }
+    setDatos({
+      ...datos,
+      [e.target.name]: value,
+    });
+  };
 
   function handleSubmit(e) {
     e.preventDefault()
 
     const data = {
-      buildingId: building,
-      userId: user,
-      floor,
-      number,
-      created_at: currentDate(),
-      updated_at: currentDate()
+      piso,
+      numero,
+      edificio,
+      propietarios,
     }
 
-    axios
-      .put(url, data)
-      .then((response) => {
-        // La respuesta del server
-        historial.push('/flats')
-      })
-      .catch((error) => {
-        console.log(error)
-      });
-  }
+  const token = localStorage.getItem('token');
+  const headers = { 'Authorization': `Bearer  ${token}` }
+  const config = { method: 'PUT', url: url_departamento, data,headers }
 
-  function handleBuilding(event) {
-    setBuilding(Number(event.target.value))
-  }
-
-  function handleUser(event) {
-    setUser(Number(event.target.value))
-  }
-
-  function handleFloor(event) {
-    setFloor(Number(event.target.value))
-  }
-
-  function handleNumber(event) {
-    setNumber(Number(event.target.value))
+  axios.request(config)
+    .then ((response) => {
+      // La respuesta del server
+      historial.push('/flats')
+    })
+    .catch((err) => {
+      console.log(err.response.data)
+      setErrors(err.response.data)
+    });
   }
 
   return (
@@ -87,44 +108,40 @@ const FlatsEdit = () => {
       <div className="FlatsEdit">
 
         <form className="m-0" onSubmit={handleSubmit}>
-          <h2 className="mb-5">Edit a flat</h2>
+          <h2 className="mb-5">Editar departamento </h2>
 
           <div className="mb-3">
-            <label className="form-label">Building</label>
-            <select className="form-select" value={building} onChange={handleBuilding}>
+            <label className="form-label">Piso</label>
+            <input type="text" className="form-control" name="piso" value={piso} onChange={actualizarState} />
+            { errors && errors.piso && <div className="error-message">{errors.piso}</div> }
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Número</label>
+            <input type="text" className="form-control" name="numero" value={numero} onChange={actualizarState} />
+            { errors && errors.numero && <div className="error-message">{errors.numero}</div> }
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Edificio</label>
+            <select className="form-select" name="edificio" value={edificio} onChange={actualizarState}>
               <option value="0">Selecciona un edificio</option>
-              {buildings.map((building, index) =>
-                <option
-                  value={building.id}
-                  key={index}>
-                  {building.name}
-                </option>
+              {edificiosData.map((edificio, index) =>
+                <option value={edificio.id} key={index}>{edificio.nombre}</option>
               )}
             </select>
+            { errors && errors.edificio && <div className="error-message">{errors.edificio}</div> }
           </div>
 
           <div className="mb-3">
-            <label className="form-label">User</label>
-            <select className="form-select" value={user} onChange={handleUser}>
-              <option value="0">Selecciona un propietario</option>
-              {users.map((user, index) =>
-                <option
-                  value={user.id}
-                  key={index}>
-                  {user.name}
-                </option>
+            <label className="form-label">Propietarios</label>
+            <select className="form-select" multiple name="propietarios" value={propietarios} onChange={actualizarState}>
+              {propietariosData.map((propietario, index) =>
+                <option value={propietario.id} key={index}>{propietario.username}</option>
               )}
             </select>
-          </div>
-
-          <div className="mb-3">
-            <label className="form-label">Floor</label>
-            <input type="text" className="form-control" value={floor} onChange={handleFloor} />
-          </div>
-
-          <div className="mb-3">
-            <label className="form-label">Number</label>
-            <input type="text" className="form-control" value={number} onChange={handleNumber} />
+            <small>Mantenga presionado "Control" o "Comando" en una Mac, para seleccionar más de uno.</small>
+            { errors && errors.propietario && <div className="error-message">{errors.propietario}</div> }
           </div>
 
           <div className="mb-3">
