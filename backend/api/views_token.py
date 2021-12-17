edad8685444fcaa785baff94d74a13fac39de441
Mariana Navarro <maronare10@gmail.com>
2021-12-17 03:40:04 -0500

    backend: se añadieron mas campos en el token para que usarlo en el frontend estos son: username y role

--- backend/api/views_token.py
+++ backend/api/views_token.py
@@ -7,9 +7,26 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
 
-        token['usuario_nombre'] = user.get_full_name() 
-        # token['condominios'] = list(user.condominio_set.values('nombre'))
-        token['condominio_nombre'] = user.condominio_set.first().nombre
+        token['usuario_nombre'] = user.get_full_name()
+        token['username'] = user.username
+
+        esAdministrador = user.groups.filter(name='administrador')
+        esPropietario = user.groups.filter(name='propietario')
+
+        if (esAdministrador.exists()):
+            token['role'] = 'administrador'
+        elif(esPropietario.exists()):
+            token['role'] = 'propietario'
+        else:    
+            token['role'] = 'desconocido'
+
+        condominio_nombre = user.condominio_set.first()
+        departamento_nombre = user.departamento_set.first()
+
+        if (condominio_nombre):
+            token['condominio_nombre'] = condominio_nombre.nombre
+        elif (departamento_nombre):
+            token['condominio_nombre'] = departamento_nombre.edificio.condominio.nombre
 
         return token
 
