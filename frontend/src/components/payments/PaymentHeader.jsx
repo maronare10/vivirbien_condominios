9de5c6a45db96088a0c64685d9d7dd8b0f5058c7
Mariana Navarro <maronare10@gmail.com>
2021-12-17 03:43:11 -0500

    frontend: se enlazó el frontend de pagos con el endpoint de pagos en el backend

--- frontend/src/components/payments/PaymentHeader.jsx
+++ frontend/src/components/payments/PaymentHeader.jsx
@@ -3,18 +3,22 @@ import IconSearch from '../../asset/icons/imag-search.svg'
 import './PaymentHeader.scss'
 
 const Search = () => {
+  const isAdministrador = () => localStorage.getItem('role') === 'administrador'
+
   return (
     <div className="contenedor-search px-5 mb-5">
-      <h1 className="paymentName">Maintenance Payments</h1>
+      <h1 className="paymentName">Pagos</h1>
 
-      <form className="contenedor-form mt-0">
+      <form className="contenedor-form mt-0" style={{ display: "none"}}>
         <span className="iconSearch">
           <img src={IconSearch} alt="Search" />
         </span>
         <input className="inputSearch" type="search" placeholder="Busca por flat number or user name" />
       </form>
 
-      <a className="botonAddPayment btn btn-primary" href="/payments/add">Add a pay</a>
+      { isAdministrador() && 
+        <a className="botonAddPayment btn btn-primary" href="/payments/add">Agregar pago</a>
+      }
     </div>
   )
 }
--- frontend/src/components/vistas/payments/PaymentsEdit.jsx
+++ frontend/src/components/vistas/payments/PaymentsEdit.jsx
@@ -10,130 +10,221 @@ import './PaymentsEdit.scss'
 const PaymentsEdit = () => {
   const historial = useHistory()
 
-  const { flats } = useApp()
-
   const { id } = useParams()
 
-  const url = `http://localhost:8000/payments/${id}`
+  const [ departamentosData, setDepartamentosData ] = useState([])
+  const [ propietariosData, setPropietariosData ] = useState([])
+  const [ serviciosData, setServiciosData ] = useState([])
 
-  const [flatId, setFlatId] = useState(0)
-  const [dueDate, setDueDate] = useState("")
-  const [amountToPay, setAmountToPay] = useState(0)
-  const [amountPaid, setAmountPaid] = useState(0)
-  const [operationNumber, setOperationNumber] = useState("")
+  const [datos, setDatos] = useState({
+    monto_a_pagar: 0,
+    monto_pagado: 0,
+    numero_operacion: "",
+    vencimiento: "",
+    voucher: "",
+    departamento: null,
+    pagado_por: null,
+    servicio: null,
+  });
+
+  const [errors, setErrors] = useState(null)
+
+  const {
+    monto_a_pagar,
+    monto_pagado,
+    numero_operacion,
+    vencimiento,
+    voucher,
+    departamento,
+    pagado_por,
+    servicio
+  } = datos;
+
+  const url_pago = `http://localhost:8000/api/pagos/${id}`
 
   useEffect(() => {
+    const url_departamentos = 'http://localhost:8000/api/departamentos'
+    const url_propietarios = 'http://localhost:8000/api/propietarios'
+    const url_servicios = 'http://localhost:8000/api/servicios'
 
-    axios
-      .get(url)
-      .then(response => {
-        setFlatId(response.data.flatId)
-        setDueDate(response.data.dueDate)
-        setAmountToPay(response.data.amountToPay)
-        setAmountPaid(response.data.amountPaid)
-        setOperationNumber(response.data.operationNumber)
+    const token = localStorage.getItem('token');
+    const headers = { 'Authorization': `Bearer ${token}` }
+
+    const config_departamentos = { method: 'GET', url: url_departamentos, headers }
+    const config_propietarios = { method: 'GET', url: url_propietarios, headers }
+    const config_servicios = { method: 'GET', url: url_servicios, headers }
+    const config_pago = { method: 'GET', url: url_pago, headers }
+
+    axios.request(config_departamentos)
+      .then((response) => {
+        // console.log(response.data.results)
+        setDepartamentosData(response.data.results)
       })
-
-  }, [url])
-
-  function currentDate() {
-    const now = new Date()
-    const day = now.getDate()
-    const month = now.getMonth()
-    const year = now.getFullYear()
-    return `${year}-${month}-${day}`
-  }
+      .catch((error) => {
+        console.log(error)
+      });
+    
+    axios.request(config_propietarios)
+      .then((response) => {
+        // console.log(response.data.results)
+        setPropietariosData(response.data.results)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+    
+    axios.request(config_servicios)
+      .then((response) => {
+        // console.log(response.data.results)
+        setServiciosData(response.data.results)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+    
+    axios.request(config_pago)
+      .then((response) => {
+        const {
+          monto_a_pagar,
+          monto_pagado,
+          numero_operacion,
+          vencimiento,
+          voucher,
+          departamento,
+          pagado_por,
+          servicio
+        } = response.data
+        console.log(response.data)
+        setDatos({
+          monto_a_pagar,
+          monto_pagado,
+          numero_operacion,
+          vencimiento,
+          voucher,
+          departamento,
+          pagado_por,
+          servicio
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }, [])
 
   function handleSubmit(e) {
     e.preventDefault()
 
     const data = {
-      flatId: flatId,
-      dueDate,
-      amountToPay,
-      amountPaid,
-      operationNumber,
-      created_at: currentDate(),
-      updated_at: currentDate()
+      monto_a_pagar: Number(monto_a_pagar),
+      monto_pagado: Number(monto_pagado),
+      numero_operacion,
+      vencimiento: vencimiento ? vencimiento : '',
+      voucher,
+      departamento: departamento ? Number(departamento) : '',
+      pagado_por: pagado_por ? Number(pagado_por) : '',
+      servicio: servicio ? Number(servicio) : '',
     }
 
-    axios
-      .put(url, data)
+    const token= localStorage.getItem('token');
+    const headers = {'Authorization': `Bearer ${token}`}
+    const config = { method: 'PUT', url: url_pago, data, headers }
+
+    axios.request(config)
       .then((response) => {
         // La respuesta del server
         historial.push('/payments')
       })
-      .catch((error) => {
-        console.log(error)
+      .catch((err) => {
+        setErrors(err.response.data)
       });
   }
 
-  function handleFlatId(event) {
-    setFlatId(Number(event.target.value))
-  }
-
-  function handleDueDate(event) {
-    setDueDate(event.target.value)
-  }
-
-  function handleAmountToPay(event) {
-    setAmountToPay(Number(event.target.value))
-  }
-
-  function handleAmountPaid(event) {
-    setAmountPaid(Number(event.target.value))
-  }
-
-  function handleOperationNumber(event) {
-    setOperationNumber(event.target.value)
-  }
+  const actualizarState = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div className="MainSeccion__content p-5 text-start">
 
-      <div className="PaymentsEdit">
+      <div className="PaymentAdd">
 
         <form className="m-0" onSubmit={handleSubmit}>
-          <h2 className="mb-5">Edit a payment</h2>
+          <h2 className="mb-5">Crear pago</h2>
+          <div className="mb-3">
+            <label className="form-label">Fecha de vencimiento</label>
+            <input type="date" className="form-control" name="vencimiento" value={vencimiento} onChange={actualizarState} />
+            { errors && errors.vencimiento && <div className="error-message">{errors.vencimiento}</div> }
+          </div>
 
           <div className="mb-3">
-            <label className="form-label">Flat</label>
-            <select className="form-select" value={flatId} onChange={handleFlatId}>
+            <label className="form-label">Departamento</label>
+            <select className="form-select" name="departamento" value={departamento} onChange={actualizarState}>
               <option value="0">Selecciona un dpto.</option>
-              {flats.map((flat, index) =>
-                <option
-                  value={flat.id}
-                  key={index}>
-                  {flat.building.name}: {flat.number}
+              {departamentosData.map((flat, index) => 
+                <option value={flat.id} key={index}>
+                  {flat.edificio_extra.nombre}: {flat.numero}
                 </option>
               )}
             </select>
+            { errors && errors.departamento && <div className="error-message">{errors.departamento}</div> }
           </div>
 
           <div className="mb-3">
-            <label className="form-label">Due date</label>
-            <input type="date" className="form-control" value={dueDate} onChange={handleDueDate} />
+            <label className="form-label">Servicio</label>
+            <select className="form-select" name="servicio" value={servicio} onChange={actualizarState}>
+              <option value="0">Selecciona un servicio.</option>
+              {serviciosData && serviciosData.map((servicio, index) => 
+                <option value={servicio.id} key={index}>
+                  {servicio.nombre}  
+                </option>
+              )}
+            </select>
+            { errors && errors.servicio && <div className="error-message">{errors.servicio}</div> }
           </div>
 
           <div className="mb-3">
-            <label className="form-label">Amount to pay</label>
-            <input type="text" className="form-control" value={amountToPay} onChange={handleAmountToPay} />
+            <label className="form-label">Monto a pagar</label>
+            <input type="text" className="form-control" name="monto_a_pagar" value={monto_a_pagar} onChange={actualizarState} />
+            { errors && errors.monto_a_pagar && <div className="error-message">{errors.monto_a_pagar}</div> }
           </div>
 
           <div className="mb-3">
-            <label className="form-label">Amount paid</label>
-            <input type="text" className="form-control" value={amountPaid} onChange={handleAmountPaid} />
+            <label className="form-label">Monto pagado</label>
+            <input type="text" className="form-control" name="monto_pagado" value={monto_pagado} onChange={actualizarState} />
+            { errors && errors.monto_pagado && <div className="error-message">{errors.monto_pagado}</div> }
           </div>
 
           <div className="mb-3">
-            <label className="form-label">Operation number</label>
-            <input type="text" className="form-control" value={operationNumber} onChange={handleOperationNumber} />
+            <label className="form-label">Número de operación</label>
+            <input type="text" className="form-control" name="numero_operacion" value={numero_operacion} onChange={actualizarState} />
+            { errors && errors.numero_operacion && <div className="error-message">{errors.numero_operacion}</div> }
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Voucher(foto)</label>
+            <input type="text" className="form-control" name="numero_operacion" value={voucher} onChange={actualizarState} />
+            { errors && errors.voucher && <div className="error-message">{errors.voucher}</div> }
+          </div>
+          
+          <div className="mb-3">
+            <label className="form-label">Pagado por</label>
+            <select className="form-select" name="pagado_por" value={pagado_por} onChange={actualizarState}>
+              <option value="0">Selecciona un propietario.</option>
+              {propietariosData.map((propietario, index) => 
+                <option value={propietario.id} key={index}>
+                  {propietario.username}  
+                </option>
+              )}
+            </select>
+            { errors && errors.pagado_por && <div className="error-message">{errors.pagado_por}</div> }
           </div>
 
           <div className="mb-3">
             <input type="submit" className="form-control btn btn-primary" />
           </div>
-
         </form>
       </div>
 
