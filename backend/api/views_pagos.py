c1f61cb3ac42d2621eb58d7e3559d8d8d75d2a4c
Mariana Navarro <maronare10@gmail.com>
2021-12-17 03:39:07 -0500

    backend: se mejoró la logica para que el administrador pueda listar pagos

--- backend/api/views_pagos.py
+++ backend/api/views_pagos.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 
-from .models import Pago
+from .models import Departamento, Pago, Edificio
 from .serializers import PagoSerializer
 
 
@@ -12,8 +12,21 @@ class PagosListCreate(generics.ListCreateAPIView):
 
     def get_queryset(self):
         propietario = self.request.user
-        departamentosDelPropietario = propietario.departamento_set.all()
-        return self.queryset.filter(departamento__in=departamentosDelPropietario)
+        
+        esAdministrador = propietario.groups.filter(name='administrador')
+        esPropietario = propietario.groups.filter(name='propietario')
+        
+        if (esAdministrador.exists()):
+            condominiosDelPropietario = propietario.condominio_set.all()
+            edificiosEnLosCondominios = Edificio.objects.filter(condominio__in=condominiosDelPropietario)
+            departamentosDeLosEdificios = Departamento.objects.filter(edificio__in=edificiosEnLosCondominios)
+            pagosDeLosDepartamentos = Pago.objects.filter(departamento__in=departamentosDeLosEdificios)
+            return pagosDeLosDepartamentos.all()
+        elif(esPropietario.exists()):
+            departamentosDelPropietario = propietario.departamento_set.all()
+            return self.queryset.filter(departamento__in=departamentosDelPropietario)
+        else:
+            return []
 
 class PagosRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pago.objects.all()
@@ -22,5 +35,18 @@ class PagosRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         propietario = self.request.user
-        departamentosDelPropietario = propietario.departamento_set.all()
-        return self.queryset.filter(departamento__in=departamentosDelPropietario)
+        
+        esAdministrador = propietario.groups.filter(name='administrador')
+        esPropietario = propietario.groups.filter(name='propietario')
+        
+        if (esAdministrador.exists()):
+            condominiosDelPropietario = propietario.condominio_set.all()
+            edificiosEnLosCondominios = Edificio.objects.filter(condominio__in=condominiosDelPropietario)
+            departamentosDeLosEdificios = Departamento.objects.filter(edificio__in=edificiosEnLosCondominios)
+            pagosDeLosDepartamentos = Pago.objects.filter(departamento__in=departamentosDeLosEdificios)
+            return pagosDeLosDepartamentos.all()
+        elif(esPropietario.exists()):
+            departamentosDelPropietario = propietario.departamento_set.all()
+            return self.queryset.filter(departamento__in=departamentosDelPropietario)
+        else:
+            return []
