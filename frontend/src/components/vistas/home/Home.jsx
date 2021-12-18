01b2a62a959f1e8ae9e432aaf401f44db621f4a5
Mariana Navarro <maronare10@gmail.com>
2021-12-17 23:29:41 -0500

    frontend: mejora en home si es admin o propietario

--- frontend/src/components/vistas/home/Home.jsx
+++ frontend/src/components/vistas/home/Home.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import "./homeStyle.scss";
 import imageBuilding from '../../../asset/icons/imag-building.svg';
+import { Link } from "react-router-dom";
 
 function Home(props) {
-  let name = localStorage.getItem('name');
+  const name = localStorage.getItem('username');
+  const isAdministrador = () => localStorage.getItem('role') === 'administrador'
+
   return (
     <div className="MainSeccion__content p-5">
-      <h2 className="MainSeccion__title">Welcome back {name}!</h2>
+      <h2 className="MainSeccion__title">Bienvenido {name}!</h2>
 
-      <div className="MainSeccion__message alert">
+      <div className="MainSeccion__message alert" style={{ display: isAdministrador() ? 'none' : '' }}>
         <span>No tiene deudas pendientes</span>
-        <button className="btn bg-condominio">See payments</button>
+
+        <Link className="btn bg-condominio" to={'/payments'}>
+          See payments
+        </Link>
       </div>
 
       <img src={imageBuilding} alt="icon-home"></img>
 
-      <div className="MainSeccion__info">
+      <div className="MainSeccion__info" style={{ display: !isAdministrador() ? 'none' : '' }}>
         <div className="MainSeccion__info__item">
           <strong>Monthly income</strong>
           <div>S/ 50,000.00</div>
